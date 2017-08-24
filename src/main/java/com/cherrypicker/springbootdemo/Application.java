4149cee3bf1a7c40a449752ade0853adf310aed5
henrylian84 <henry.j.lian@gmail.com>
2017-08-24 00:54:57 -0700

    initial springboot gradle

---
+++ src/main/java/com/cherrypicker/springbootdemo/Application.java
@@ -0,0 +1,14 @@
+package com.cherrypicker.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by henrylian on 8/23/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
